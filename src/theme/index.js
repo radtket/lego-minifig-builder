a548bc50d098ad72219644ce4ccb0361b26084fc
Taylor Radtke <taylorradtke@gmail.com>
2024-12-01 13:12:31 -0600

    node 20

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./GlobalStyles";
@@ -27,6 +28,10 @@ const Theme = ({ children }) => {
       {children}
     </ThemeProvider>
   );
+};
+
+Theme.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Theme;
